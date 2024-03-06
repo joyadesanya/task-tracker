bb80103158509af4f671fdad9d59c7236055beb7
Joy Adesanya <posiogunti@gmail.com>
2024-03-06 13:22:35 +0100

    Build the task list

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,33 @@
+import { useState } from "react";
+import TaskList from "./components/TaskList";
+
 const App = () => {
-  return <div>App</div>;
+  const [tasks, setTasks] = useState([
+    {
+      id: 1,
+      description: "Finish reading React.js Documentation",
+      category: "Learning",
+      status: "Completed",
+    },
+    {
+      id: 2,
+      description: "Do a 30-minute workout session",
+      category: "Fitness",
+      status: "Ongoing",
+    },
+    {
+      id: 3,
+      description: "Complete and submit the weekly coding challenge",
+      category: "Work",
+      status: "Completed",
+    },
+  ]);
+  return (
+    <TaskList
+      tasks={tasks}
+      onDelete={(id) => setTasks(tasks.filter((task) => task.id !== id))}
+    />
+  );
 };
 
 export default App;
---
+++ src/components/TaskList.tsx
@@ -0,0 +1,63 @@
+import {
+  Table,
+  Thead,
+  Tbody,
+  Tr,
+  Th,
+  Td,
+  TableCaption,
+  TableContainer,
+  Button,
+  Checkbox,
+} from "@chakra-ui/react";
+
+interface Task {
+  id: number;
+  description: string;
+  category: string;
+  status: string;
+}
+
+interface Props {
+  tasks: Task[];
+  onDelete: (id: number) => void;
+}
+
+const TaskList = ({ tasks, onDelete }: Props) => {
+  return (
+    <TableContainer>
+      <Table>
+        <TableCaption textAlign="left">Tasks List</TableCaption>
+        <Thead>
+          <Tr>
+            <Th>Description</Th>
+            <Th>Category</Th>
+            <Th>Status</Th>
+          </Tr>
+        </Thead>
+
+        <Tbody>
+          {tasks.map((task) => (
+            <Tr key={task.id}>
+              <Td>{task.description}</Td>
+              <Td>{task.category}</Td>
+              <Td>
+                <Checkbox>Completed</Checkbox>
+              </Td>
+              <Td>
+                <Button
+                  colorScheme="whatsapp"
+                  onClick={() => onDelete(task.id)}
+                >
+                  Delete
+                </Button>
+              </Td>
+            </Tr>
+          ))}
+        </Tbody>
+      </Table>
+    </TableContainer>
+  );
+};
+
+export default TaskList;
