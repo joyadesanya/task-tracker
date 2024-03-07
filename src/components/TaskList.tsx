613e9bdbba9711064ee057447b7ffbed1d318f1a
Joy Adesanya <posiogunti@gmail.com>
2024-03-07 11:30:10 +0100

    Wrap Table in a Box

--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -8,6 +8,7 @@ import {
   TableCaption,
   Button,
   Checkbox,
+  Box,
 } from "@chakra-ui/react";
 
 interface Task {
@@ -23,33 +24,44 @@ interface Props {
 
 const TaskList = ({ tasks, onDelete }: Props) => {
   return (
-    <Table>
-      <TableCaption textAlign="left">Tasks List</TableCaption>
-      <Thead>
-        <Tr>
-          <Th>Description</Th>
-          <Th>Category</Th>
-          <Th>Status</Th>
-        </Tr>
-      </Thead>
-
-      <Tbody>
-        {tasks.map((task) => (
-          <Tr key={task.id}>
-            <Td>{task.description}</Td>
-            <Td>{task.category}</Td>
-            <Td>
-              <Checkbox>Completed</Checkbox>
-            </Td>
-            <Td>
-              <Button colorScheme="whatsapp" onClick={() => onDelete(task.id)}>
-                Delete
-              </Button>
-            </Td>
+    <Box
+      width={[
+        "100%", // 0-30em
+        "100%", // 30em-48em
+        "100%", // 48em-62em
+        "100%", // 62em+
+      ]}
+    >
+      <Table>
+        <TableCaption textAlign="left">Tasks List</TableCaption>
+        <Thead>
+          <Tr>
+            <Th>Description</Th>
+            <Th>Category</Th>
+            <Th>Status</Th>
           </Tr>
-        ))}
-      </Tbody>
-    </Table>
+        </Thead>
+        <Tbody>
+          {tasks.map((task) => (
+            <Tr key={task.id}>
+              <Td>{task.description}</Td>
+              <Td>{task.category}</Td>
+              <Td>
+                <Checkbox>Completed</Checkbox>
+              </Td>
+              <Td>
+                <Button
+                  colorScheme="whatsapp"
+                  onClick={() => onDelete(task.id)}
+                >
+                  Delete
+                </Button>
+              </Td>
+            </Tr>
+          ))}
+        </Tbody>
+      </Table>
+    </Box>
   );
 };
 
